8a1779c6a6d15e363ce2c9b891cb61c6978cdb9e
neverlast1 <joelb5266@gmail.com>
2020-05-31 18:56:18 +1000

    Added commercialProperty() function, 31/05/2020 Joel Busch

--- insurancePremiumCalculator/insurancePremiumCalculator.py
+++ insurancePremiumCalculator/insurancePremiumCalculator.py
@@ -1,26 +1,106 @@
 import math
 
+# TO DO
+# Implement Loop to allow multiple claim/user input
+# Implement lists to track number of each client type input (P/C)
+# Implement lists to track total premiums of each type (P/C)
+# Implement error checking
+
 def privateHousing():
     premiumRate = 0
     premiumCharged = 0
-    amountInsured = int(input("How much are you getting insured? "))
+    amountInsured = int(input("Amount Insured ($) ? "))
+    proCharge = 50
+    numClaims = int(input("Number of claims ? "))
+    noClaimBonusRate = 0.1
+    noClaimBonus = 0
 
     if amountInsured < 100000:
         premiumRate = 0.003
 
-        premiumCharged = amountInsured * premiumRate
+        premiumCharged = (amountInsured * premiumRate) + proCharge
 
     elif amountInsured >= 100000:
         premiumRate = 0.0025
 
-        premiumCharged = amountInsured * premiumRate
+        premiumCharged = (amountInsured * premiumRate) + proCharge
 
-    return premiumCharged
+    if numClaims == 0:
+        noClaimBonus = premiumCharged * noClaimBonusRate
 
-#use of a main() function to keep code a little more clean.
-#I am not considering the main() function as one of the 2 defined and called functions.
+        print("\nPremium Summary")
+        print("Amount Insured = " + "$" + str(amountInsured))
+        print("Number of Claims = " + str(numClaims))
+        print("No Claim Bonus = " + "$" + str(noClaimBonus))
+        print("Premium = " + "$" + str(premiumCharged - noClaimBonus))
+        return premiumCharged
+
+    elif numClaims >= 1:
+        print("\nPremium Summary")
+        print("Amount Insured = " + "$" + str(amountInsured))
+        print("Number of Claims = " + str(numClaims))
+        print("Premium = " + "$" + str(premiumCharged + noClaimBonus))
+        return premiumCharged
+
+
+
+
+def commercialProperty():
+    premiumRate = 0
+    premiumCharged = 0
+    amountInsured = int(input("Amount Insured ($) ? "))
+    proCharge = 80
+    numClaims = int(input("Number of claims ? "))
+    noClaimBonusRate = 0.15
+    noClaimBonus = 0
+
+    if amountInsured < 250000:
+        premiumRate = 0.005
+
+        premiumCharged = (amountInsured * premiumRate) + proCharge
+
+    elif amountInsured >= 250000:
+        premiumRate = 0.0075
+
+        premiumCharged = (amountInsured * premiumRate) + proCharge
+
+    if numClaims == 0:
+        noClaimBonus = premiumCharged * noClaimBonusRate
+
+        print("\nPremium Summary")
+        print("Amount Insured = " + "$" + str(amountInsured))
+        print("Number of Claims = " + str(numClaims))
+        print("No Claim Bonus = " + "$" + str(noClaimBonus))
+        print("Premium = " + "$" + str(premiumCharged - noClaimBonus))
+        return premiumCharged
+
+    elif numClaims >= 1:
+        print("\nPremium Summary")
+        print("Amount Insured = " + "$" + str(amountInsured))
+        print("Number of Claims = " + str(numClaims))
+        print("Premium = " + "$" + str(premiumCharged + noClaimBonus))
+        return premiumCharged
+
+# use of a main() function to keep code a little more clean.
+# I am not considering the main() function as one of the 2 defined and called functions.
+
+
 def main():
-    print(privateHousing())
+    try:
+        usrType = input("User type(P/C)? ")
+
+        if usrType == "P" or usrType == "p":
+            privateHousing()
+
+        elif usrType == "C" or usrType == "c":
+            commercialProperty()
+
+        else:
+            print("Please enter a valid user type")
+    except:
+        print("An Error has occurred")
+
 
 if __name__ == "__main__":
     main()
+
